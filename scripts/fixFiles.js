e103334b23f6a877530c40f2f9cd72793b7a1e12
Victor Teo <victor.teo@fillmula.com>
2023-03-17 12:07:23 +0800

    remove unused code

--- scripts/fixFiles.js
+++ scripts/fixFiles.js
@@ -2,5 +2,4 @@ const { readFileSync, writeFileSync } = require('fs')
 
 let content = readFileSync('./index.js')
 content += "\nglobalThis.require = require\nprocess.on('SIGINT', function() { process.exit(0) })\n"
-//content += "\nconst { receiveRequire } = nativeBinding\nreceiveRequire(require)\n"
 writeFileSync("./index.js", content)

99537915db6f0ce94d0a286ebf6c4d716152017a
Victor Teo <victor.teo@fillmula.com>
2023-03-17 18:47:21 +0800

    reformat try.js

--- try.js
+++ try.js
@@ -1,17 +1,18 @@
 const { App } = require("./index.js")
 const Decimal = require('decimal.js')
 
-async function main() {
-  const app = new App()
-  app.transform("replaceComWithIo", async function (input) {
-    return input.replace(/com$/, 'io')
-  })
-  app.transform("10times", async function (input) {
-    return Decimal((input || Decimal(5)) * 10)
-  })
-  app.validate("oddLength", (s) => s.length % 2 == 0)
-  app.callback("print", (v) => console.log(v))
-  await app.run()
-}
+const app = new App()
 
-main()
+app.transform("replaceComWithIo", async function (input) {
+  return input.replace(/com$/, 'io')
+})
+
+app.transform("10times", async function (input) {
+  return Decimal((input || Decimal(5)) * 10)
+})
+
+app.validate("oddLength", (s) => s.length % 2 == 0)
+
+app.callback("print", (v) => console.log(v))
+
+app.run()

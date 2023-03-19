e6917d9a370cb99f21086c23a662eb663eecb756
Victor Teo <victor.teo@fillmula.com>
2023-03-19 16:29:55 +0800

    update

--- scripts/fixFiles.js
+++ scripts/fixFiles.js
@@ -1,5 +1,8 @@
 const { readFileSync, writeFileSync } = require('fs')
 
-let content = readFileSync('./index.js')
-content += "\nglobalThis.require = require\nprocess.on('SIGINT', function() { process.exit(0) })\n"
-writeFileSync("./index.js", content)
+let arg = process.argv[process.argv.length - 1];
+if (arg === 'index.js') {
+  let content = readFileSync('./index.js')
+  content += "\nglobalThis.require = require\nprocess.on('SIGINT', function() { process.exit(0) })\n"
+  writeFileSync("./index.js", content)
+}

0c41bd6b876990e26da9215386770d2bc480d86e
Victor Teo <victor.teo@fillmula.com>
2024-01-09 17:36:00 +0800

    teo model object to js any

--- src/object/model.rs
+++ src/object/model.rs
@@ -1,6 +1,9 @@
 use napi::{Env, JsUnknown, Result};
 use teo::prelude::model;
 
+use crate::dynamic::js_model_object_from_teo_model_object;
+
 pub fn teo_model_object_to_js_any(model_object: &model::Object, env: &Env) -> Result<JsUnknown> {
-    unreachable!()
+    let js_object = js_model_object_from_teo_model_object(*env, model_object.clone())?;
+    Ok(js_object.into_unknown())
 }

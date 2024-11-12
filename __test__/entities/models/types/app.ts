1dc4f1a011f37b8a07cd77cc0122828cc25be3fa
Victor Teo <victor.teo@fillmula.com>
2024-11-12 22:27:08 +0800

    segmentation fault

--- __test__/entities/models/types/app.ts
+++ __test__/entities/models/types/app.ts
@@ -4,19 +4,21 @@ import { SupportCreateInput, SupportFindManyArgs, Teo } from "./entities"
 
 export default function loadApp() {
     const app = new App(schemaPathArgs(__filename, "schema.teo"))
-    app.mainNamespace().defineHandler("myCreateObject", async (req: Request) => {
-        const teo: Teo = req.teo()
-        const input: SupportCreateInput = req.bodyObject()
-        const object = await teo.support.createObject(input)
-        await object.save()
-        return Response.data(await object.toTeon())
-    })
-    app.mainNamespace().defineHandler("myFindManyObjects", async (req: Request) => {
-        const teo: Teo = req.teo()
-        const input: SupportFindManyArgs = req.bodyObject()
-        const objects = await teo.support.findManyObjects(input)
-        const values = Promise.all(objects.map(async object => await object.toTeon()))
-        return Response.data(values)
+    app.mainNamespace().defineModelHandlerGroup("Support", (group) => {
+        group.defineHandler("myCreateObject", async (req: Request) => {
+            const teo: Teo = req.teo()
+            const input: SupportCreateInput = req.bodyObject()
+            const object = await teo.support.createObject(input)
+            await object.save()
+            return Response.data(await object.toTeon())
+        })
+        group.defineHandler("myFindManyObjects", async (req: Request) => {
+            const teo: Teo = req.teo()
+            const input: SupportFindManyArgs = req.bodyObject()
+            const objects = await teo.support.findManyObjects(input)
+            const values = Promise.all(objects.map(async object => await object.toTeon()))
+            return Response.data(values)
+        })
     })
     return app
 }

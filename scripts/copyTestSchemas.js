b74ba953399ce66e5f1db041ee1078547c3751da
Victor Teo <victor.teo@fillmula.com>
2024-11-17 20:43:01 +0800

    update tests

--- scripts/copyTestSchemas.js
+++ scripts/copyTestSchemas.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const { globSync } = require('glob')
 const path = require('path')
 
-globSync('__test__/**/*.{teo,jpg,txt}').forEach((src) => {
-    const dest = path.join('__test_build__', src.replace(/^__test__\/?\\?/, ''))
+globSync('tests/**/*.{teo,jpg,txt}').forEach((src) => {
+    const dest = path.join('tests_build', src.replace(/^tests\/?\\?/, ''))
     const destDir = path.dirname(dest)
     if (!fs.existsSync(destDir)) {
         fs.mkdirSync(destDir, { recursive: true })

0ef8ab65dd260fd46dbd33cdd5bc840ffd13ef04
Victor Teo <victor.teo@fillmula.com>
2024-11-19 13:33:06 +0800

    local values

--- src/request/local/values.rs
+++ src/request/local/values.rs
@@ -1,7 +1,39 @@
-use napi::{Env, JsObject, JsUnknown, Result};
-use teo::prelude::request::local_values::LocalValues as TeoLocalValues;
+use napi::{Env, JsUnknown, Result};
+use teo::prelude::{request::local_values::LocalValues as TeoLocalValues, Value};
 
 #[napi(js_name = "LocalValues")]
 pub struct LocalValues {
     pub(crate) teo_local_values: TeoLocalValues,
+}
+
+#[napi]
+impl LocalValues {
+
+    #[napi]
+    pub fn insert(&self, key: String, value: JsUnknown, env: Env) -> Result<()> {
+        let value = crate::object::js_any_to_teo_value(value, env)?;
+        self.teo_local_values.insert(key, value);
+        Ok(())
+    }
+
+    #[napi]
+    pub fn get(&self, key: String, env: Env) -> Result<JsUnknown> {
+        let value: &Value = self.teo_local_values.get(&key)?;
+        Ok(crate::object::value::teo_value_to_js_any_no_app_data(value, &env)?)
+    }
+
+    #[napi]
+    pub fn contains(&self, key: String) -> bool {
+        self.teo_local_values.contains(&key)
+    }
+
+    #[napi]
+    pub fn remove(&self, key: String) {
+        self.teo_local_values.remove(key.as_str());
+    }
+
+    #[napi]
+    pub fn clear(&self) {
+        self.teo_local_values.clear();
+    }
 }

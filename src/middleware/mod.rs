d66ad2fece24eb8beb32d21edb8dbe21ab8d5705
Victor Teo <victor.teo@fillmula.com>
2024-12-04 21:19:55 +0800

    fix

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -16,7 +16,7 @@ impl FromNapiValue for SendMiddlewareCallback {
     unsafe fn from_napi_value(env: napi::sys::napi_env, napi_val: napi::sys::napi_value) -> napi::Result<Self> {
         let callback = JsFunction::from_napi_value(env, napi_val)?;
         let threadsafe_callback: ThreadsafeFunction<(teo::prelude::Request, Next), ErrorStrategy::Fatal> = callback.create_threadsafe_function(0, |ctx: ThreadSafeCallContext<(teo::prelude::Request, Next)>| {
-            let request_ctx = Request::new(ctx.value.0.clone());
+            let request_ctx = Request::from(ctx.value.0.clone());
             let request_ctx_unknown = request_ctx.into_instance(ctx.env)?.as_object(ctx.env).into_unknown();
             let teo_next = ctx.value.1;
             let wrapped_next = ctx.env.create_function_from_closure("next", move |_| {

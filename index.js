e215b5dc38b4af689e41786380c5036813d8389d
Victor Teo <victor.teo@fillmula.com>
2024-12-05 18:48:35 +0800

    fix

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ Request.prototype[customInspectSymbol] = function(_, inspectOptions) {
   return "Request " + inspect({
     "method": this.method,
     "path": this.path,
-    "queryString": this.queryString,
+    "query": this.query,
     "contentType": this.contentType,
     "headers": this.headers,
     "bodyObject": this.bodyObject,
